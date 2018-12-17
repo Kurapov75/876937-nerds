d820d540aab7d657af7324d8efdf9f888420e600
Kurapov75 <kurapov1975@gmail.com>
2018-12-18 00:52:51 +1100

    Модальное окно
    Добавлена анимация и проверка на заполнение полей

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var open = document.querySelector(".modal-open");
 var popup = document.querySelector(".modal");
 var close = popup.querySelector(".modal-close");
+var form = popup.querySelector(".message");
+var name = popup.querySelector("#name-field");
+var email = popup.querySelector("#email-field");
 
 open.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -11,3 +14,22 @@ close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
 });
+
+form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+  });
+
+form.addEventListener("submit", function (evt) {
+    if (!name.value || !email.value) {
+      evt.preventDefault();
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
